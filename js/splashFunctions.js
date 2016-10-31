b81b378e8eb29d639f5e4f5776baea15557346be
valiz user <contact@template01.info>
2016-10-31 17:00:13 +0100

    updates

--- js/splashFunctions.js
+++ js/splashFunctions.js
@@ -2,7 +2,7 @@ function splash_loadSplash() {
     $.ajax('http://api.template-studio.nl/wp-json/wp/v2/pages?filter[name]=index&fields=acf', {
         success: function(data) {
             indexblurb = data[0].acf.indexblurb
-            $("#splashPage").append('<div class="textBlurb animated fadeIn"><p>'+indexblurb+'</p></div>');
+            $("#splashPage").append('<div class="textBlurb animated fadeIn"><p id="fittexttextBlurb" >'+indexblurb+'</p></div>');
 
         },
         error: function() {
